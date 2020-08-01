cbeb70fa0e3c9236a25b902946adc11dae4af1e9
derpmachine1 <ricksliu.mail@gmail.com>
2020-08-01 19:41:40 -0400

    Store RGB values of smaller version of image

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from PIL import Image
 
-FILE = "test.jpg"
+FILE = "test2.jpg"
 
 image = Image.open(FILE)
 pixels = image.load()
-print(image.size)
-print(pixels[0,0])
+
+w = image.size[0]
+h = image.size[1]
+
+w_out = 180
+h_out = w_out * h / w
+
+rgb = list()
+
+x = 0
+while x < w_out:
+    y = 0
+    column = list()
+    while y < h_out:
+        column.append(pixels[int(float(x) * w / w_out), int(float(y) * h / h_out)][:3])
+        y += 1
+    rgb.append(column)
+    x += 1
+
+# for col in rgb:
+#     print(col)
