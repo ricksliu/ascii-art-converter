a741809d5269322763a106793e283d559e161238
derpmachine1 <ricksliu.mail@gmail.com>
2020-09-20 16:02:14 -0400

    Frames stop flashing up and down, frame rate more precise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from time import sleep
+import time
 from image_to_ascii import generate_ascii
 
 PATH = "frames/"
-WIDTH = 180
+WIDTH = 100
 FRAME_RATE = 24
 
 # Loops and generates ASCII art for each frame
@@ -23,8 +23,11 @@ except FileNotFoundError:
 input("ASCII generation finished. Generated " + str(frame + 1) + " frames. Press enter.")
 
 # Prints according to FRAME_RATE
-for a in ascii:
-    print(a)
-    sleep(1.0 / FRAME_RATE)
+a = 0
+start_time = time.time()
+while a < len(ascii):
+    if time.time() - start_time >= a / FRAME_RATE:
+        print('\n' + ascii[a], end='')
+        a += 1
 
-input("Process finished. Press enter.")
+input("\n\nProcess finished. Press enter.")
