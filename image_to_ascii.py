0ff9f3cf60b3863b9e0cd18be2cf8c71500c72c7
derpmachine1 <ricksliu.mail@gmail.com>
2020-10-13 22:28:47 -0400

    Optimized ascii conversion code

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -7,84 +7,74 @@ def generate_ascii(file, width):
     image = Image.open(file)
     pixels = image.load()
 
-    # Inputted image's dimensions
-    w = image.size[0]
-    h = image.size[1]
+    # Number of rows of characters; halved since characters are taller than they are wide
+    height = width * image.size[1] / image.size[0] / 2
 
-    # Outputted dimensions
-    w_out = width
-    h_out = w_out * h / w / 2  # Height is halved since characters are taller than they are wide
-
-    # 2D list of RGB values of pixels, scaled to outputted dimensions
+    # Creates 2D list of RGB values of pixels, scaled to required dimensions
     rgb = list()
-    for y in range(int(h_out)):
+    for y in range(int(height)):
         row = list()
-        for x in range(w_out):
-            row.append(pixels[int(float(x) * w / w_out), int(float(y) * h / h_out)][:3])
+        for x in range(width):
+            row.append(pixels[int(float(x) * image.size[0] / width), int(float(y) * image.size[1] / height)][:3])
         rgb.append(row)
 
-    # Creates 2D list of ascii characters based on RGB values
-    ascii = list()
+    # Creates string of ascii characters based on RGB values
+    output = ""
     for y in rgb:
-        row = list()
+        output += "\n"
+
         for pixel in y:
             # If (reasonably) gray
             average_rgb = (pixel[0] + pixel[1] + pixel[2]) / 3.0
             if abs(pixel[0] - average_rgb) < 50 and abs(pixel[1] - average_rgb) < 50 and abs(pixel[2] - average_rgb) < 50:
                 if 0 <= average_rgb < 51:
-                    row.append(" ")
+                    output += " "
                 elif 51 <= average_rgb < 102:
-                    row.append(".")
+                    output += "."
                 elif 102 <= average_rgb < 153:
-                    row.append("=")
+                    output += "="
                 elif 153 <= average_rgb < 204:
-                    row.append("?")
+                    output += "?"
                 else:
-                    row.append("@")
+                    output += "@"
 
             # If (reasonably) red
             elif pixel[0] >= pixel[1] and pixel[0] >= pixel[2]:
                 if 0 <= pixel[0] < 51:
-                    row.append(" ")
+                    output += " "
                 elif 51 <= pixel[0] < 102:
-                    row.append(",")
+                    output += ","
                 elif 102 <= pixel[0] < 153:
-                    row.append("*")
+                    output += "*"
                 elif 153 <= pixel[0] < 204:
-                    row.append("/")
+                    output += "/"
                 else:
-                    row.append("#")
+                    output += "#"
 
             # If (reasonably) green
             elif pixel[1] >= pixel[0] and pixel[1] >= pixel[2]:
                 if 0 <= pixel[1] < 51:
-                    row.append(" ")
+                    output += " "
                 elif 51 <= pixel[1] < 102:
-                    row.append("`")
+                    output += "`"
                 elif 102 <= pixel[1] < 153:
-                    row.append("+")
+                    output += "+"
                 elif 153 <= pixel[1] < 204:
-                    row.append("\\")
+                    output += "\\"
                 else:
-                    row.append("$")
+                    output += "$"
 
             # If (reasonably) blue
-            elif pixel[2] >= pixel[0] and pixel[2] >= pixel[1]:
+            else:
                 if 0 <= pixel[2] < 51:
-                    row.append(" ")
+                    output += " "
                 elif 51 <= pixel[2] < 102:
-                    row.append("'")
+                    output += "'"
                 elif 102 <= pixel[2] < 153:
-                    row.append("-")
+                    output += "-"
                 elif 153 <= pixel[2] < 204:
-                    row.append("|")
+                    output += "|"
                 else:
-                    row.append("&")
-
-        ascii.append(row)
-
-    # Creates single string rather than many lines so the entire image is printed instantaneously
-    output = ""
-    for row in ascii:
-        output += "\n" + "".join(row)
+                    output += "&"
+        
     return output
