83aa0198a5727b022f7dbf3bf286e082af144a74
rick-liu-02 <ricksliu.mail@gmail.com>
2022-09-06 22:48:39 -0400

    Refine font size calculation and text align

--- main.py
+++ main.py
@@ -1,42 +1,85 @@
 import os
 import glob
 import json
-from math import ceil
 import numpy as np
 import cv2
 from PIL import Image, ImageDraw, ImageFont
 from moviepy.editor import VideoFileClip
 
+# Find font with size that will fit string perfectly across screen
+def get_font(font_path, num_chars, target_width):
+    sample_text = "X" * num_chars
+
+    # Make two size guesses and record widths
+    prev_font_size = 12
+    font = ImageFont.truetype(font_path, prev_font_size)
+    prev_width = font.getsize(sample_text)[0]
+    curr_font_size = 24
+    font = ImageFont.truetype(font_path, curr_font_size)
+    curr_width = font.getsize(sample_text)[0]
+
+    while True:
+        # Interpolate next size guess from previous two guesses
+        next_font_size = int(prev_font_size + (target_width - prev_width) * (curr_font_size - prev_font_size) / (curr_width - prev_width))
+        if next_font_size == prev_font_size or next_font_size == curr_font_size:
+            next_font_size = int((prev_font_size + curr_font_size) / 2)
+
+        # If cannot refine further, return
+        if next_font_size == prev_font_size or next_font_size == curr_font_size:
+            return ImageFont.truetype(font_path, next_font_size)
+
+        # Record next width
+        font = ImageFont.truetype(font_path, next_font_size)
+        next_width = font.getsize(sample_text)[0]
+
+        # Keep closest two guesses
+        closest_array = [abs(w - target_width) for w in [prev_width, curr_width, next_width]]
+        if closest_array[0] == max(closest_array):
+            prev_font_size = next_font_size
+            prev_width = next_width
+        elif closest_array[1] == max(closest_array):
+            curr_font_size = next_font_size
+            curr_width = next_width
+
+# Get ratio of height of char to width
+def get_font_height_to_width(font):
+    num_lines = 100
+    sample_text = "\n".join(["X"] * num_lines)
+
+    draw = ImageDraw.Draw(Image.new('RGB', (100, 100)))
+    width, height = draw.textsize(sample_text, font)
+    return height / width / num_lines
+
 # Convert cv2 image to ASCII string
-def image_to_ascii(input_image, output_pattern, output_width, sx):
+def image_to_ascii(input_image, ascii_pattern, ascii_length, font_height_to_width):
     input_width = input_image.shape[1]
     input_height = input_image.shape[0]
-    output_height = ceil(float(output_width) * input_height / input_width * 0.55)
-    output_ascii = ""
+
+    output_height = int(ascii_length * input_height / input_width / font_height_to_width)
+    output_ascii = []
 
     # For each sampled pixel in image
     for h in range(int(output_height)):
-        for w in range(int(output_width)):
+        for w in range(int(ascii_length)):
             # Calculate brightness
-            x = int(w * input_width / output_width)
-            y = int(h * input_height / output_height)
-            bgr = input_image[y, x].tolist()
-            brightness = float(bgr[0] + bgr[1] + bgr[2]) / 3
+            bgr = input_image[int(h * input_height / output_height), int(w * input_width / ascii_length)].tolist()
+            brightness = (bgr[0] + bgr[1] + bgr[2]) / 3
 
             # Append corresponding ASCII character
-            output_pattern_ix = int(brightness * len(output_pattern) / 256)
-            output_ascii += output_pattern[output_pattern_ix]
+            ascii_pattern_ix = int(brightness * len(ascii_pattern) / 256)
+            output_ascii.append(ascii_pattern[ascii_pattern_ix])
 
-        output_ascii += "\n"
+        output_ascii.append("\n")
 
-    return output_ascii
+    return ("".join(output_ascii))[:-1]
 
 # Convert ASCII string to cv2 image
-def ascii_to_image(input_ascii, output_width, output_height, output_font):
-    # Create black PIL image and write text
-    output_image_pil = Image.new("RGB", (output_width, output_height), color=(0, 0, 0))
+def ascii_to_image(input_ascii, image_width, image_height, font):
+    # Create black PIL image and write text; cv2 does not support custom fonts
+    output_image_pil = Image.new("RGB", (image_width, image_height), color=(0, 0, 0))
     draw = ImageDraw.Draw(output_image_pil)
-    draw.text((0, 0), input_ascii, (255, 255, 255), font=output_font)
+    ascii_width, ascii_height = draw.textsize(input_ascii, font)
+    draw.text(((image_width - ascii_width) / 2, (image_height - ascii_height) / 2), input_ascii, (255, 255, 255), font=font)
 
     # Convert to cv2 image
     output_image = np.array(output_image_pil)
@@ -49,8 +92,8 @@ with open(config_path) as file:
     config = json.load(file)
     INPUT_PATH = glob.glob(config["inputPath"])
     OUTPUT_PATH = config["outputPath"]
-    OUTPUT_PATTERN = config["outputPattern"][::-1]
-    OUTPUT_WIDTH = config["outputWidth"]
+    ASCII_PATTERN = config["asciiPattern"]
+    ASCII_LENGTH = config["asciiLength"]
 
 # For each video in input directory
 for input_path in INPUT_PATH:
@@ -65,14 +108,16 @@ for input_path in INPUT_PATH:
     output_path = OUTPUT_PATH + file_name
     temp_output_path = OUTPUT_PATH + "temp_" + file_name
     output_video = cv2.VideoWriter(temp_output_path, 0, -1, frame_rate, (width, height))
-    font = ImageFont.truetype("consolas.ttf", ceil(1.81 * width / OUTPUT_WIDTH))
+
+    font = get_font("consolas.ttf", ASCII_LENGTH, width)
+    font_height_to_width = get_font_height_to_width(font)
 
     # For each frame in input video
     success, input_image = input_video.read()
     curr_frame = 1
     while success:
         # Generate ASCII frame and write to output video
-        output_ascii = image_to_ascii(input_image, OUTPUT_PATTERN, OUTPUT_WIDTH, curr_frame)
+        output_ascii = image_to_ascii(input_image, ASCII_PATTERN, ASCII_LENGTH, font_height_to_width)
         output_image = ascii_to_image(output_ascii, width, height, font)
         output_video.write(output_image)
 
